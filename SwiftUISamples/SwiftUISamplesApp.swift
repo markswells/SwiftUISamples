f49befd42273d351fca6497f18a1aaf45a4f1ec8
Mark Wells <mark.s.wells@gmail.com>
2023-11-21 15:36:53 +0100

    Initial Commit

---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Mark Wells on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
